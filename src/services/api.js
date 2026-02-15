ffb241e076fccc8d9504908a5bf919b20d3b5cb0
crisurubu <cristianourubu@gmail.com>
2026-02-14 22:03:07 -0300

    Conectando o frontend ao backend do render

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,13 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api'
+  // MUDANÇA AQUI: Agora aponta para o servidor real da ONG
+  baseURL: 'https://sistema-castracao-api.onrender.com/api'
 });
 
-// AQUI É ONDE O ERRO 403 MORRE:
-// Este interceptor "carimba" o token em toda chamada que você fizer usando 'api'
 api.interceptors.request.use(config => {
-    // 1. Busca o token que o Login salvou no seu localStorage
     const token = localStorage.getItem('token');
     
-    // 2. Se a chave existir, coloca ela no envelope (header) da requisição
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
